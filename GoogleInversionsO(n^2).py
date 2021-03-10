7870bd90d0ee34751c39e0f148bd71ae85cfeb45
vishvedula <45387278+vishvedula@users.noreply.github.com>
2021-03-10 17:54:45 +0530

    GoogleInversionsO(n^2).py --> did in O(N^2)
    LEVEL: Medium
    
    Good morning! Here's your coding interview problem for today.
    
    This problem was asked by Google.
    
    We can determine how "out of order" an array A is by counting the number of inversions it has. Two elements A[i] and A[j] form an inversion if A[i] > A[j] but i < j. That is, a smaller element appears after a larger element.
    
    Given an array, count the number of inversions it has. Do this faster than O(N^2) time.
    
    You may assume each element in the array is distinct.
    
    For example, a sorted list has zero inversions. The array [2, 4, 1, 3, 5] has three inversions: (2, 1), (4, 1), and (4, 3). The array [5, 4, 3, 2, 1] has ten inversions: every distinct pair forms an inversion.

---
+++ GoogleInversionsO(n^2).py
@@ -0,0 +1,25 @@
+'''
+
+LEVEL: Medium
+
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Google.
+
+We can determine how "out of order" an array A is by counting the number of inversions it has. Two elements A[i] and A[j] form an inversion if A[i] > A[j] but i < j. That is, a smaller element appears after a larger element.
+
+Given an array, count the number of inversions it has. Do this faster than O(N^2) time.
+
+You may assume each element in the array is distinct.
+
+For example, a sorted list has zero inversions. The array [2, 4, 1, 3, 5] has three inversions: (2, 1), (4, 1), and (4, 3). The array [5, 4, 3, 2, 1] has ten inversions: every distinct pair forms an inversion.
+'''
+array = [2, 4, 1, 3, 5] 
+result =0
+print('lenght of array is: ',len(array))
+for i in range(0,len(array)):
+    for j in range(i+1, len(array)):
+        if array[i]>array[j]:
+            result = result+1
+
+print(f'final result is {result}')
